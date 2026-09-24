243646b13b721fc2b3faa4ae10505b5e6aa2a4ca
Victoria <finkovika2412006@gmail.com>
2026-09-24 21:53:47 +0300

    Implement student B algorithm

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "shared_types.h"
+
+int main() {
+    auto data = std::make_shared<const InputData>(
+        InputData{ [](double x) { return x * x; }, 0.0, 10.0, 1000 }
+    );
+    auto resultB = calculateTrapezoidal(data);
+    auto [valB, evalB] = *resultB;
+    std::cout << "Trapezoidal: " << valB << "\n";
+    return 0;
+}
